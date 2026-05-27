e01d1ed5b4cc88a36f6866684324d32121425861
abdallhlw <bdallhlw62@gmail.com>
2026-05-27 16:12:15 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+app.use(cors());
 const http = require('http'); 
 const socketIo = require('socket.io');
 const multer = require('multer');
